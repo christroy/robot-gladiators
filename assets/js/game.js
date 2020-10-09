9905e936c0d3b723e81aa881d7d7a443be6cc8c2
POLYMATH INTL <POLYMATH@MacBook-Pro.local>
2020-10-09 18:22:01 -0500

    modified js file by adding functions and console.log and saw how this is applied via inspect element > console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,48 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
 // this creates a function named "fight"
 
 function fight() {
     window.alert("The fight has begun!");
   }
+
+  // fight();
+
+//examples// 
+
+  sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+  sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+
+  /* This is a comment */
+
+
+
+// a comment can work like this OR //
+/* 
+It
+can
+work
+like
+this for longer comments we can wrap them like this. 
+Ideally, try to keep a comment short and use the // syntax
+to keep your code neat and tidy/refined/short. 
+*/
